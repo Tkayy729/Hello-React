e00e8df00485589dad4034829f2267b448ba38db
Tkayy729 <emmanueltweneboah71@gmail.com>
2025-10-01 00:20:52 +0200

    episode 01

--- App.js
+++ App.js
@@ -1,18 +1,15 @@
-// const heading = React.createElement(
-//   "h1",
-//   { classNamed: "heading", attri: "value" },
-//   "Hello Namaste Javascript"
-// );
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement(
     "div",
-    { id: "child1" },
+    { id: "child1", key: "1" },
     React.createElement(
       "div",
-      { id: "child2" },
+      { id: "child2", key: "1"  },
       React.createElement("h1", { id: "h1tag" }, "I am the h1 tag")
     )
   ),
