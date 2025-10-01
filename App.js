1a074ff2e9f87e8f9c1d51bb57fa216a3962dbe2
Tkayy729 <emmanueltweneboah71@gmail.com>
2025-10-01 17:42:57 +0200

    put react element in react component

--- App.js
+++ App.js
@@ -3,13 +3,21 @@ import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const Title = () => {
+const title = () => {
   return <h1>This is the title</h1>;
 };
+
+const reactEl = (
+    <div>
+        <h1>dsnfnf</h1>
+        <h1>dsnfnf</h1>
+    </div>
+)
 
 const App = () => (
   <div className="container">
     <title />
+    {reactEl}
     <h1>I am re-learning react</h1>
   </div>
 );
