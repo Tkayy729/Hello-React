466cf1a424fb3ab919d9b325ca2ed733ad3f16e0
Tkayy729 <emmanueltweneboah71@gmail.com>
2025-10-02 22:28:41 +0200

    restaurants data

---
+++ assets/restaurants.js
@@ -0,0 +1,92 @@
+export const restaurants = [
+  {
+    id: 1,
+    name: "McDonald's",
+    location: "Pardubice, Brno, Prague",
+    rating: 4.4,
+    time: "38 minutes",
+    image:
+      "https://imageproxy.wolt.com/menu/menu-images/603659d62deccf067d86f8d0/8d390732-49f2-11ef-a982-ce592682695d_tpo_cze_del_6380_may24.jpg?w=600",
+  },
+  {
+    id: 2,
+    name: "KFC",
+    location: "Prague, Ostrava, Brno",
+    rating: 4.2,
+    time: "32 minutes",
+    image:
+      "https://imageproxy.wolt.com/menu/menu-images/603659d62deccf067d86f8d0/8d390732-49f2-11ef-a982-ce592682695d_tpo_cze_del_6380_may24.jpg?w=600",
+  },
+  {
+    id: 3,
+    name: "Burger King",
+    location: "Brno, Ostrava",
+    rating: 4.1,
+    time: "40 minutes",
+    image:
+      "https://imageproxy.wolt.com/menu/menu-images/603659d62deccf067d86f8d0/8d390732-49f2-11ef-a982-ce592682695d_tpo_cze_del_6380_may24.jpg?w=600",
+  },
+  {
+    id: 4,
+    name: "Subway",
+    location: "Prague, Brno",
+    rating: 4.3,
+    time: "29 minutes",
+    image:
+      "https://imageproxy.wolt.com/menu/menu-images/603659d62deccf067d86f8d0/8d390732-49f2-11ef-a982-ce592682695d_tpo_cze_del_6380_may24.jpg?w=600",
+  },
+  {
+    id: 5,
+    name: "Domino's Pizza",
+    location: "Prague, Pardubice",
+    rating: 4.5,
+    time: "35 minutes",
+    image:
+      "https://imageproxy.wolt.com/menu/menu-images/603659d62deccf067d86f8d0/8d390732-49f2-11ef-a982-ce592682695d_tpo_cze_del_6380_may24.jpg?w=600",
+  },
+  {
+    id: 6,
+    name: "Pizza Hut",
+    location: "Brno, Prague",
+    rating: 4.0,
+    time: "42 minutes",
+    image:
+      "https://imageproxy.wolt.com/menu/menu-images/603659d62deccf067d86f8d0/8d390732-49f2-11ef-a982-ce592682695d_tpo_cze_del_6380_may24.jpg?w=600",
+  },
+  {
+    id: 7,
+    name: "Starbucks",
+    location: "Prague, Brno",
+    rating: 4.6,
+    time: "25 minutes",
+    image:
+      "https://imageproxy.wolt.com/menu/menu-images/603659d62deccf067d86f8d0/8d390732-49f2-11ef-a982-ce592682695d_tpo_cze_del_6380_may24.jpg?w=600",
+  },
+  {
+    id: 8,
+    name: "Costa Coffee",
+    location: "Pardubice, Prague",
+    rating: 4.3,
+    time: "27 minutes",
+    image:
+      "https://imageproxy.wolt.com/menu/menu-images/603659d62deccf067d86f8d0/8d390732-49f2-11ef-a982-ce592682695d_tpo_cze_del_6380_may24.jpg?w=600",
+  },
+  {
+    id: 9,
+    name: "Dunkin' Donuts",
+    location: "Brno, Ostrava",
+    rating: 4.1,
+    time: "30 minutes",
+    image:
+      "https://imageproxy.wolt.com/menu/menu-images/603659d62deccf067d86f8d0/8d390732-49f2-11ef-a982-ce592682695d_tpo_cze_del_6380_may24.jpg?w=600",
+  },
+  {
+    id: 10,
+    name: "Papa John's Pizza",
+    location: "Prague, Brno",
+    rating: 4.2,
+    time: "36 minutes",
+    image:
+      "https://imageproxy.wolt.com/menu/menu-images/603659d62deccf067d86f8d0/8d390732-49f2-11ef-a982-ce592682695d_tpo_cze_del_6380_may24.jpg?w=600",
+  },
+];
