66fa46974acfc1388c265d7c05e7cd3136100fb8
Tkayy729 <emmanueltweneboah71@gmail.com>
2025-10-02 22:29:42 +0200

    header and body component created in same file

--- App.js
+++ App.js
@@ -1,25 +1,67 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { restaurants } from "./assets/restaurants";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const Title = () => {
-  return <h1>This is the title</h1>;
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://marketplace.canva.com/EAGKDhXEhoY/1/0/1600w/canva-brown-and-white-simple-modern-professional-catering-logo-Dvz9NG3gqk0.jpg"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
-const reactEl = (
-    <div>
-        <h1>dsnfnf</h1>
-        <h1>dsnfnf</h1>
+const RestaurantCard = (props) => {
+  const { name, location, image, time } = props;
+  return (
+    <div className="res-card">
+      <img src={image} alt="res-log" className="res-logo" />
+      <h3>{name}</h3>
+      <h4>{location}</h4>
+      <h4>{rating}</h4>
+      <h4>{time}</h4>
     </div>
-)
+  );
+};
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        {restaurants.map((res, index) => (
+          <RestaurantCard
+            key={index}
+            name={res.name}
+            location={res.location}
+            rating={res.rating}
+            time={res.time}
+            image={res.image}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-const App = () => (
-  <div className="container">
-    <Title />
-    {reactEl}
-    <h1>I am re-learning react</h1>
+const AppLayout = () => (
+  <div className="app">
+    <Header />
+    <Body />
   </div>
 );
 
-root.render(<App />);
+root.render(<AppLayout />);
