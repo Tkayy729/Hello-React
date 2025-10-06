143ccf877ac75911526d8bb77337a641b11a95b7
Tkayy729 <emmanueltweneboah71@gmail.com>
2025-10-06 16:43:03 +0200

    toggle login/logout, learning the power of state variables

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
+  const [btnName, setBtnname] = useState("login");
   return (
     <div className="header">
       <div className="logo-container">
@@ -12,6 +14,14 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button
+            className="login-btn"
+            onClick={() =>
+              btnName === "login" ? setBtnname("logout") : setBtnname("login")
+            }
+          >
+            {btnName}
+          </button>
         </ul>
       </div>
     </div>
