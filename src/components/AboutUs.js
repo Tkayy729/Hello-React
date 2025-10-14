43d3344d5c98487263b7e98aefe69c47aa48d125
Tkayy729 <emmanueltweneboah71@gmail.com>
2025-10-15 00:54:51 +0200

    display user card

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,7 +1,12 @@
+import User from "./User";
+import UserClass from "./UserClass";
+
 const AboutUs = () => {
   return (
     <div>
       <h1>About Us</h1>
+      <User />
+      <UserClass name= {"Emmanuel class"}/>
     </div>
   );
 };
