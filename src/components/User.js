266f89f5de132ed0cae0b5840acfe7c23946ca6f
Tkayy729 <emmanueltweneboah71@gmail.com>
2025-10-15 00:53:37 +0200

    user class functional component

---
+++ src/components/User.js
@@ -0,0 +1,11 @@
+const User = () => {
+  return (
+    <div className="user-card">
+      <h2>Name: Emmanuel</h2>
+      <h3>Location: Brno</h3>
+      <h4>Contact: @tkayyLFC</h4>
+    </div>
+  );
+};
+
+export default User;
