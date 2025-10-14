db7ae7df22a5db8e9a2c9f643353c81853e5fb54
Tkayy729 <emmanueltweneboah71@gmail.com>
2025-10-15 00:54:19 +0200

    api change, will do later"

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,47 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router";
-import { MENU_API } from "../utils/constants";
 
 const RestaurantMenu = () => {
-  const [resMenuInfo, setResMenuInfo] = useState(null);
-  const { resId } = useParams();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=670");
-      console.log(res)
-    
-    }
-    fetchData();
-  }, [resId]);
 
-  
-
-  // const {
-  //   name,
-  //   areaName,
-  //   locality,
-  //   cuisines = [],
-  //   costForTwoMessage,
-  //   avgRatingString,
-  //   sla: { deliveryTime } = {},
-  //   aggregatedDiscountInfo: { header } = {},
-  // } = resMenuInfo;
-
-  return (
-    // <div>
-    //   <h1>{name}</h1>
-    //   <p>Cuisines: {cuisines.join(", ")}</p>
-    //   <p>
-    //     Locality: {locality}, Area name: {areaName}
-    //   </p>
-    //   <p>cost for two: {costForTwoMessage}</p>
-    //   <p>Rating⭐ :{avgRatingString}</p>
-    //   <p>🕒 Delivery Time: {deliveryTime} mins</p>
-    //   <p>{header}</p>
-    // </div>
+  return (  
     <div>
-      Name of restaurant
+      <h1>
+        Under construction
+      </h1>
     </div>
   );
 };
