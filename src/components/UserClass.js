cb32d0cf9e40e77a1753bc88d16ffcbebb0f77af
Tkayy729 <emmanueltweneboah71@gmail.com>
2025-10-17 03:03:04 +0200

    fetch data in class component

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,13 +4,32 @@ class UserClass extends React.Component {
   constructor(props) {
     super(props);
     console.log(props);
+
+    this.state ={
+        userInfo:{
+            name: "",
+            location: "Brno",
+            url:"",
+        avatar_url:""
+        }
+        
+    }
+  }
+
+  async componentDidMount(){
+    const data = await fetch("https://api.github.com/users/tkayy729");
+    const json = await data.json();
+    console.log("json", json)
+    this.setState({userInfo: json});
   }
   render() {
+    const { login, location , url , avatar_url} = this.state.userInfo;
     return (
       <div className="user-card">
-        <h2>Name: {this.props.name}</h2>
-        <h3>Location: Brno</h3>
-        <h4>Contact: @tkayyLFC</h4>
+        <img src ={avatar_url}/>
+        <h2>Name: {login}</h2>
+        <h3>Location: {location || "BRNO"}</h3>
+        <h4>Contact: {url}</h4>
       </div>
     );
   }
