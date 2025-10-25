3319c98b9c8cd8ad09d74ed48d1e1d5a83bfc577
Tkayy729 <emmanueltweneboah71@gmail.com>
2025-10-25 16:35:01 +0200

    custom hook

---
+++ src/utils/useOnlineStatus.js
@@ -0,0 +1,22 @@
+const { useState, useEffect } = require("react");
+
+const useOnlineStatus = () => {
+  const [onlineStatus, setOnlineStatus] = useState(navigator.onLine);
+
+  useEffect(() => {
+    const goOnline = () => setOnlineStatus(true);
+    const goOffline = () => setOnlineStatus(false);
+
+    window.addEventListener("online", goOnline);
+    window.addEventListener("offline", goOffline);
+
+    return () => {
+      window.removeEventListener("online", goOnline);
+      window.removeEventListener("offline", goOffline);
+    };
+  }, []);
+
+  return onlineStatus;
+};
+
+export default useOnlineStatus;
