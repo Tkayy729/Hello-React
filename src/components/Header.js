a3ec8afe11d1ae2e44d2fe1c3a887396927792bf
Tkayy729 <emmanueltweneboah71@gmail.com>
2025-10-25 16:36:04 +0200

    added a useOnlineStatus hook

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Header = () => {
   const [btnName, setBtnname] = useState("login");
+  const isOnline = useOnlineStatus();
+
   return (
     <div className="header">
       <div className="logo-container">
@@ -11,6 +14,7 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
+         <li>Online Status {isOnline ? "✅" : "🔴"}</li> 
           <li>
             <Link to={"/"}>Home</Link>
           </li>
