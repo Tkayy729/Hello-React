6ade2906f120e8577b269dd229e60654c86aeb97
Tkayy729 <emmanueltweneboah71@gmail.com>
2025-11-02 21:28:51 +0100

    header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,18 @@
-import { useState } from "react";
-import { LOGO_URL } from "../utils/constants";
+import { MdLogout } from "react-icons/md";
 import { Link } from "react-router-dom";
 import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Header = () => {
-  const [btnName, setBtnname] = useState("login");
   const isOnline = useOnlineStatus();
 
   return (
-    <div className="header">
-      <div className="logo-container">
-        <img className="logo" src={LOGO_URL} />
+    <div className="flex justify-between bg-gray-800 text-gray-50 px-5 shadow-lg">
+      <div className="flex place-items-center">
+        <span className="text-gray-400 ">TkayRES</span>
       </div>
-      <div className="nav-items">
-        <ul>
-         <li>Online Status {isOnline ? "✅" : "🔴"}</li> 
+      <div className="nav-items-middle">
+        <ul className="flex p-4 m-4 space-x-10">
+          <li>Online Status {isOnline ? "✅" : "🔴"}</li>
           <li>
             <Link to={"/"}>Home</Link>
           </li>
@@ -28,15 +26,10 @@ const Header = () => {
             <Link to={"/contact-us"}>Contact Us</Link>
           </li>
           <li>Cart</li>
-          <button
-            className="login-btn"
-            onClick={() =>
-              btnName === "login" ? setBtnname("logout") : setBtnname("login")
-            }
-          >
-            {btnName}
-          </button>
         </ul>
+      </div>
+      <div className="nav-items-right place-content-center">
+        <MdLogout className="w-6 h-6 "/>
       </div>
     </div>
   );
