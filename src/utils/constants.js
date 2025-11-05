1efe6d05d3be3bd164316420431f0811a2b05f40
Tkayy729 <emmanueltweneboah71@gmail.com>
2025-11-05 12:07:50 +0100

    added base url and menu api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,9 @@ export const ALT_IMG_URL =
   "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0d/cf/4f/ef/chow-mien-cantonisese.jpg?w=900&h=500&s=1";
 
 export const LOGO_URL =
-  "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png";
+  "https://logowik.com/content/uploads/images/food-service4537.jpg";
 
 export const MENU_API =
-"https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId="
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
+
+export const BASE_URL = "https://namastedev.com/api/v1";
